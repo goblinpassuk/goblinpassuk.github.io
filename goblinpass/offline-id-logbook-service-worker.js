f8cf6f58694c1423e2581ca90f3738d190775598
goblinpassuk <unused-ion-diary@duck.com>
2026-06-29 15:31:21 +0100

    gg

---
+++ goblinpass/offline-id-logbook-service-worker.js
@@ -0,0 +1,41 @@
+const CACHE_NAME = "goblinpass-offline-id-logbook-v2";
+const APP_FILES = [
+  "./offline-id-logbook.html",
+  "./offline-id-logbook.webmanifest",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_FILES)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys()
+      .then((names) => Promise.all(
+        names
+          .filter((name) => name.startsWith("goblinpass-offline-id-logbook-") && name !== CACHE_NAME)
+          .map((name) => caches.delete(name))
+      ))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") return;
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        if (response.ok && new URL(event.request.url).origin === self.location.origin) {
+          const copy = response.clone();
+          caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
+        }
+        return response;
+      })
+      .catch(() => caches.match(event.request).then((cached) => (
+        cached || (event.request.mode === "navigate" ? caches.match("./offline-id-logbook.html") : Response.error())
+      )))
+  );
+});
